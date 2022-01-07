0b21b21145d7e2580056a1d73e73ce250061f132
unknown <weihaozhaung@gmail.com>
2022-01-07 17:17:32 +0800

    intialized packages

--- src/scripts/tiltAnimation.js
+++ src/scripts/tiltAnimation.js
@@ -1,4 +1,4 @@
-import VanillaTilt from "vanilla-tilt";
+import VanillaTilt from "../../node_modules/vanilla-tilt";
 
 export default function initTiltAnimation() {
   const elements = document.querySelectorAll(".js-tilt");
